293eb7e0f122b765b5ac5ab169e9f202aaa547ed
Your Github User Namemlestrella843 <email@example.commarial.estrella.843@gmail.com>
2024-09-18 18:05:03 -0400

    Extracting max sentiment

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,16 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj =  { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json=myobj, headers=headers)
-    return response.text
+
+    # Parse the response from the API to variable res
+    res = json.loads(response.text)
+    
+    # Formatting the output
+    formatted_output = res['emotionPredictions'][0]['emotion']
+    
+    # Find the dominant emotion by finding the key with maximiu value
+    dominant_emotion = max(formatted_output, key=lambda x: formatted_output[x])
+
+    #return the dominant emotion as a string
+    return dominant_emotion
+
